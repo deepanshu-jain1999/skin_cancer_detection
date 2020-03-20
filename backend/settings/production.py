c5780f7a0cd529b1e6b0afc015c2fbcf741111c7
Deepanshu Jain <deepanshuj1999@gmail.com>
2020-03-20 12:39:59 +0530

    Update static path

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -20,8 +20,7 @@ DATABASES = {
 }
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
-
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
