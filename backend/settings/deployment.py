710c6c1bc95e0b81fb67d81428cdf60ae6d2a38e
Deepanshu Jain <deepanshuj1999@gmail.com>
2020-05-31 12:23:07 +0530

    make it better

--- backend/settings/deployment.py
+++ backend/settings/deployment.py
@@ -8,7 +8,7 @@ SECRET_KEY = "@xn8l3lq=05u^i47a^dnrpr86qen0$-#@y^i0@l*+c9#_l0x^f"
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.43.185", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["skin-cancer-detection.herokuapp.com", "localhost", "127.0.0.1"]
 
 DATABASES = {
     "default": {
